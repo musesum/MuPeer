6213a4af147d0bcef775c04a95a2f4237ee60773
musesum <musesum@gmail.com>
2023-08-09 13:25:26 +0800

    add MuPeerTest

---
+++ MuPeerTest/MuPeerTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MuPeerTest
+//
+//  Created by warren on 8/9/23.
+//
+
+import SwiftUI
+import MuPeer
+
+struct ContentView: View {
+    public var peersVm = PeersVm.shared
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            PeersView(peersVm)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
